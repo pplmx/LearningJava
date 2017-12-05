635f385f6cbeeb1a90021421afb5e7f69ed6eeae
mystic_cg <215104920@qq.com>
2017-12-05 17:08:57 +0800

    List&Set 'of' function

---
+++ src/main/java/individual/cy/learn/Collection.java
@@ -0,0 +1,23 @@
+package individual.cy.learn;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: mystic
+ * @date: 2017/12/5 16:06
+ * @since: JDK1.8.0_144
+ * @version: X
+ * Description:
+ */
+public class Collection {
+    public static void main(String[] args) {
+        // 不可变,不可继续添加元素
+        Set<Integer> set = Set.of(1,2,12,23,3);
+        List<String> list = List.of("hello","world");
+        System.out.println("set = " + set);
+        System.out.println("list = " + list);
+    }
+}
