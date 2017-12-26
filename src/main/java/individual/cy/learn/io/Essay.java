d8fa31bbdcd8d854f3055d754e61cbf4c48ebd39
mystic_cg <215104920@qq.com>
2017-12-26 09:47:38 +0800

    Exercise

---
+++ src/main/java/individual/cy/learn/io/Essay.java
@@ -0,0 +1,54 @@
+package individual.cy.learn.io;
+
+import java.io.*;
+import java.util.Objects;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: mystic
+ * @date: 2017/12/26 9:04
+ * @since: JDK9.0.1
+ * @version: X
+ * Description:
+ */
+public class Essay {
+
+    private static boolean isOkay = false;
+
+    public static boolean copyFile(File source, File target) {
+        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(source));
+             BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(target))) {
+            int len;
+            byte[] bytes = new byte[1024];
+            while ((len = bis.read()) != -1) {
+                bos.write(bytes,0,len);
+            }
+            bos.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean copy(File source, File target) {
+        // 创建文件路径
+        File newFile = new File(target, source.getName());
+        // 这里的target,只当做目录处理
+        if (!target.exists()) {
+            isOkay = target.mkdirs();
+        }
+        if (source.isDirectory()) {
+            isOkay = newFile.mkdirs();
+            File[] files = source.listFiles();
+            for (File file : Objects.requireNonNull(files)) {
+                isOkay = copy(file, newFile);
+            }
+        } else {
+            isOkay = copy(source, newFile);
+        }
+        return isOkay;
+    }
+
+}
