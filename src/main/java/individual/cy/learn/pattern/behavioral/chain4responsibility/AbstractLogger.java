bb4c311083ccb8a7bba22a89bb7f9e182f14e6d0
mystic <215104920@qq.com>
2020-06-03 20:46:23 +0800

    chain of responsibility

--- src/main/java/individual/cy/learn/pattern/behavioral/chain4responsibility/AbstractLogger.java
+++ src/main/java/individual/cy/learn/pattern/behavioral/chain4responsibility/AbstractLogger.java
@@ -18,11 +18,12 @@ public abstract class AbstractLogger {
     }
 
     public void logMessage(int level, String message) {
-        if (this.level <= level) {
+        if (this.level == level) {
             write(message);
-        }
-        if (nextLogger != null) {
-            nextLogger.logMessage(level, message);
+        } else {
+            if (nextLogger != null) {
+                nextLogger.logMessage(level, message);
+            }
         }
     }
 
